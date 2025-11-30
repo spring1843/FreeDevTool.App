4683d44e4326ebd41bc08d3a72a9cbe9aea28d23
Ryan Maleki <spring1843@users.noreply.github.com>
2025-11-30 15:11:31 -0800

    Refactor playback button icon rendering logic

--- client/src/pages/tools/microphone-test.tsx
+++ client/src/pages/tools/microphone-test.tsx
@@ -469,7 +469,13 @@ export default function MicrophoneTest() {
                   tooltip={
                     isPlaying ? "Stop playback" : "Play the recorded audio"
                   }
-                  icon={isPlaying ? <Pause className="w-4 h-4 mr-2" /> : <Play className="w-4 h-4 mr-2" />}
+                  icon={
+                    isPlaying ? (
+                      <Pause className="w-4 h-4 mr-2" />
+                    ) : (
+                      <Play className="w-4 h-4 mr-2" />
+                    )
+                  }
                   className="flex-1"
                 >
                   {isPlaying ? "Stop Playback" : "Play Recording"}
